ae3711d9ee61be4cd2860bb92856523e3017f62d
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-03-24 00:34:00 -0600

    added language codes model, and partial views and templates

--- languages/models.py
+++ languages/models.py
@@ -15,7 +15,7 @@ class Language(models.Model):
 
 class LanguageCode(models.Model):
     language = models.ForeignKey(Language, on_delete=models.DO_NOTHING)
-    code = models.CharField(max_length=5)
+    code = models.CharField(max_length=5, unique=True)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- languages/views.py
+++ languages/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from languages.models import Language, LanguageCode
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.contrib import messages
 
@@ -67,6 +68,12 @@ class LanguageCodeListView(PermissionRequiredMixin, ListView):
         qs.filter(language_id=self.kwargs['language_id'])
         return qs
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        c = super(LanguageCodeListView, self).get_context_data()
+        c['language'] = get_object_or_404(Language, id=self.kwargs['language_id'])
+        print(c)
+        return c
+
 
 class LanguageCodeView(PermissionRequiredMixin, DetailView):
     permission_required = 'languages.view_languagecode'
@@ -74,11 +81,34 @@ class LanguageCodeView(PermissionRequiredMixin, DetailView):
     pk_url_kwarg = 'language_code_id'
     login_url = reverse_lazy('pages:login')
 
+    def get_context_data(self, **kwargs):
+        c = super(LanguageCodeView, self).get_context_data()
+        return c
 
-class LanguageCodeCreateView(PermissionRequiredMixin, UpdateView):
+
+class LanguageCodeCreateView(PermissionRequiredMixin, CreateView):
     permission_required = 'languages.add_languagecode'
     model = LanguageCode
     login_url = reverse_lazy('pages:login')
+    fields = ('code', 'description')
+
+    def get_context_data(self, **kwargs):
+        c = super(LanguageCodeCreateView, self).get_context_data()
+        c['language'] = get_object_or_404(Language, id=self.kwargs['language_id'])
+        c['action'] = 'Create'
+        return c
+
+    def form_valid(self, form):
+        language = get_object_or_404(Language, id=self.kwargs['language_id'])
+        form.instance.language = language
+        return super(LanguageCodeCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        messages.success(self.request, 'Language code: "%s" for language: "%s" has been added.' % (
+            self.object.code, self.object.language.name
+        ))
+        return reverse_lazy('languages:language_code', kwargs=dict(language_code_id=self.object.pk),
+                            language_id=self.object.language_id)
 
 
 class LanguageCodeEditView(PermissionRequiredMixin, UpdateView):
