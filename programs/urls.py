2162bedff50e00ca273acbec76171ae5325626ef
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-03-25 20:46:50 -0600

    Programs and levels with all views and templates

--- programs/urls.py
+++ programs/urls.py
@@ -5,8 +5,13 @@ app_name = 'programs'
 
 urlpatterns = [
     path('', views.ProgramListView.as_view(), name='program_list'),
+    path('new_program/', views.ProgramCreateView.as_view(), name='program_create'),
     path('<int:program_id>/', views.ProgramDetailView.as_view(), name='program_detail'),
     path('<int:program_id>/edit/', views.ProgramUpdateView.as_view(), name='program_edit'),
     path('<int:program_id>/delete/', views.ProgramDeleteView.as_view(), name='program_delete'),
-    path('new_program/', views.ProgramCreateView.as_view(), name='program_create')
+    path('<int:program_id>/levels/', views.LevelListView.as_view(), name='level_list'),
+    path('<int:program_id>/new_level/', views.LevelCreateView.as_view(), name='level_create'),
+    path('<int:program_id>/level/<int:level_id>', views.LevelDetailView.as_view(), name='level_detail'),
+    path('<int:program_id>/level/<int:level_id>/edit/', views.LevelUpdateView.as_view(), name='level_edit'),
+    path('<int:program_id>/level/<int:level_id>/delete/', views.LevelDeleteView.as_view(), name='level_delete')
 ]
--- programs/views.py
+++ programs/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import get_object_or_404, Http404
+from programs.models import Program, Level
 from django.urls import reverse_lazy
 from django.contrib import messages
-from programs.models import Program
 
 
 class ProgramListView(PermissionRequiredMixin, ListView):
@@ -17,6 +18,11 @@ class ProgramDetailView(PermissionRequiredMixin, DetailView):
     model = Program
     login_url = reverse_lazy('pages:login')
     pk_url_kwarg = 'program_id'
+
+    def get_context_data(self, **kwargs):
+        c = super(ProgramDetailView, self).get_context_data()
+        c['levels'] = self.object.level_set.all()[:5]
+        return c
 
 
 class ProgramCreateView(PermissionRequiredMixin, CreateView):
@@ -68,3 +74,99 @@ class ProgramDeleteView(PermissionRequiredMixin, DeleteView):
     def get_success_url(self):
         messages.success(self.request, 'Program with name: "%s" has been deleted.' % self.object.name)
         return reverse_lazy('programs:program_list')
+
+
+class LevelListView(PermissionRequiredMixin, ListView):
+    permission_required = 'programs.view_level'
+    model = Level
+    login_url = reverse_lazy('pages:login')
+    paginate_by = 10
+
+    def get_queryset(self):
+        qs = super(LevelListView, self).get_queryset()
+        print(qs)
+        qs = qs.filter(program_id=self.kwargs['program_id'])
+        return qs
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        c = super(LevelListView, self).get_context_data()
+        c['program'] = get_object_or_404(Program, id=self.kwargs['program_id'])
+        return c
+
+
+class LevelDetailView(PermissionRequiredMixin, DetailView):
+    permission_required = 'programs.view_level'
+    model = Level
+    login_url = reverse_lazy('pages:login')
+    pk_url_kwarg = 'level_id'
+
+    def get_context_data(self, **kwargs):
+        c = super(LevelDetailView, self).get_context_data()
+        if self.object.program_id != self.kwargs['program_id']:
+            raise Http404
+        c['program'] = get_object_or_404(Program, id=self.kwargs['program_id'])
+        return c
+
+
+class LevelCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = 'programs.add_level'
+    model = Level
+    login_url = reverse_lazy('pages:login')
+    fields = ('name', 'description', 'assign_min', 'assign_max')
+
+    def get_context_data(self, **kwargs):
+        c = super(LevelCreateView, self).get_context_data()
+        c['action'] = 'Create'
+        c['program'] = get_object_or_404(Program, id=self.kwargs['program_id'])
+        return c
+
+    def form_valid(self, form):
+        form.instance.program = get_object_or_404(Program, id=self.kwargs['program_id'])
+        return super(LevelCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        messages.success(self.request, 'Level with name: "%s" has been added.' % self.object.name)
+        return reverse_lazy('programs:level_detail', kwargs=dict(level_id=self.object.pk,
+                                                                 program_id=self.object.program_id))
+
+
+class LevelUpdateView(PermissionRequiredMixin, UpdateView):
+    permission_required = 'programs.change_level'
+    model = Level
+    login_url = reverse_lazy('pages:login')
+    fields = ('name', 'description', 'assign_min', 'assign_max')
+    pk_url_kwarg = 'program_id'
+
+    def get_context_data(self, **kwargs):
+        c = super(LevelUpdateView, self).get_context_data()
+        c['action'] = 'Edit'
+        if self.object.program_id != self.kwargs['program_id']:
+            raise Http404
+        c['program'] = get_object_or_404(Program, id=self.kwargs['program_id'])
+        return c
+
+    def get_success_url(self):
+        messages.success(self.request, 'Level with name: "%s" has been updated.' % self.object.name)
+        return reverse_lazy('programs:level_detail', kwargs=dict(level_id=self.object.pk,
+                                                                 program_id=self.object.program_id))
+
+
+class LevelDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = 'programs.delete_level'
+    template_name = 'programs/level_form.html'
+    model = Level
+    login_url = reverse_lazy('pages:login')
+    pk_url_kwarg = 'program_id'
+
+    def get_context_data(self, **kwargs):
+        c = super(LevelDeleteView, self).get_context_data()
+        c['action'] = 'Delete'
+        c['delete_message'] = 'Are you sure to delete: "%s" ?' % self.object.name
+        if self.object.program_id != self.kwargs['program_id']:
+            raise Http404
+        c['program'] = get_object_or_404(Program, id=self.kwargs['program_id'])
+        return c
+
+    def get_success_url(self):
+        messages.success(self.request, 'Level with name: "%s" has been deleted.' % self.object.name)
+        return reverse_lazy('programs:level_list', kwargs=dict(program_id=self.object.program_id))
