4f721347d354fe49f73f2821afb10e3ecba61040
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-03-29 23:07:15 -0600

    Changed routes and permissions, and redirects.

--- areas/urls.py
+++ areas/urls.py
@@ -4,9 +4,9 @@ from areas import views
 app_name = 'areas'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='index'),
-    path('new/', views.NewAreaView.as_view(), name='new'),
-    path('<int:area_id>/', views.AreaView.as_view(), name='area'),
-    path('<int:area_id>/edit/', views.EditAreaView.as_view(), name='edit'),
-    path('<int:area_id>/delete/', views.DeleteAreaView.as_view(), name='delete')
+    path('', views.HomeView.as_view(), name='area_list'),
+    path('new/', views.NewAreaView.as_view(), name='create_area'),
+    path('<int:area_id>/', views.AreaView.as_view(), name='area_detail'),
+    path('<int:area_id>/edit/', views.EditAreaView.as_view(), name='edit_area'),
+    path('<int:area_id>/delete/', views.DeleteAreaView.as_view(), name='delete_area')
 ]
