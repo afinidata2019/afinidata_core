56f8d95ac8f6f622ef05dda48dd6db25bf7177f9
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-04-01 20:36:14 -0600

    Changed view and template to group detail

--- groups/admin.py
+++ groups/admin.py
@@ -1,5 +1,8 @@
+from groups.models import Group, Code, AssignationMessengerUser, BotAssignation, ProgramAssignation
 from django.contrib import admin
-from groups.models import Group, Code
 
 admin.site.register(Group)
 admin.site.register(Code)
+admin.site.register(AssignationMessengerUser)
+admin.site.register(ProgramAssignation)
+admin.site.register(BotAssignation)
