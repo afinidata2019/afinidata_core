90ed1c3a2b56c9b5f0164d7ffa276c2780f393ce
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-04-01 19:03:19 -0600

    Added first permissions to groups and messenger users. And remove views without use.

--- messenger_users/urls.py
+++ messenger_users/urls.py
@@ -5,8 +5,5 @@ app_name = 'messenger_users'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
-    path('by_group/<str:group>/', views.ByGroupView.as_view(), name='by_group'),
-    path('<int:id>/', views.UserView.as_view(), name='user'),
-    path('<int:id>/data/<int:attribute_id>/edit/', views.EditAttributeView.as_view(), name='attribute_edit'),
-    path('<int:id>/data/<int:attribute_id>/delete/', views.DeleteAttributeView.as_view(), name='attribute_delete')
+    path('<int:id>/', views.UserView.as_view(), name='user')
 ]
