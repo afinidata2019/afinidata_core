75375b880b13d91d1ae1abbb5b521973980ca0eb
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-04-12 17:16:28 -0600

    Jwt working with pem

--- scripts/test_pem.py
+++ scripts/test_pem.py
@@ -5,13 +5,11 @@ import os
 
 
 def run():
-    key = jwk.JWK.generate(kty='RSA', size=2048)
-    print('key: ', key)
-    priv_pem = key.export_to_pem(private_key=True, password=None)
-    print('priv pem: ', priv_pem)
+    key_uri = os.path.join(BASE_DIR, 'afinidata.key')
+    key_file = pem.parse_file(key_uri)
+    key_string = str(key_file[0]).encode()
     payload = {'foo': 'bar', 'wup': 90}
-    priv_key = jwk.JWK.from_pem(priv_pem)
-    print('priv key: ', priv_key)
+    priv_key = jwk.JWK.from_pem(key_string)
     token = jwt.generate_jwt(payload, priv_key, 'RS256', datetime.timedelta(minutes=5))
     print('token: ', token)
     header, claims = jwt.verify_jwt(token, priv_key, ['RS256'])
