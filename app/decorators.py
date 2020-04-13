809dcb42237432190cbccfd0cda2691a4fbc241d
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-04-13 00:00:45 -0600

    Added basic auth for requests with os variables.

--- app/decorators.py
+++ app/decorators.py
@@ -1,16 +1,20 @@
-import base64
 from django.http import HttpResponse
+import base64
+import os
 
 
 def check_authorization(view_func):
 
     def wrap(request, *args, **kwargs):
-        auth = None
         try:
             auth = request.META['HTTP_AUTHORIZATION']
         except Exception as e:
             return HttpResponse('<h1>Unauthorized</h1>', status=403)
         encoded_credentials = request.META['HTTP_AUTHORIZATION'].split(' ')[1]
+        decoded_credentials = base64.b64decode(encoded_credentials).decode("utf-8").split(':')
+        if decoded_credentials[0] != os.getenv('AUTH_USER') or decoded_credentials[1] != os.getenv('AUTH_PASS'):
+            return HttpResponse('<h1>Unauthorized</h1>', status=403)
+
         return view_func(request, *args, **kwargs)
 
     return wrap
