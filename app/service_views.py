e2177c39b315bc5aa87036b51faa1adfcb74bb9e
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-04-12 23:41:16 -0600

    Added decorator to check auth header in view.

--- app/service_views.py
+++ app/service_views.py
@@ -1,8 +1,16 @@
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView
-from app import models
+from app import (
+    models,
+    decorators
+)
 
 
+@method_decorator(decorators.check_authorization, name='dispatch')
 class SignUpView(CreateView):
     model = models.User
     fields = ('first_name', 'last_name', 'password', 'identifier')
     template_name = 'app/form.html'
+
+    def get(self, request, *args, **kwargs):
+        return super(SignUpView, self).get(request, *args, **kwargs)
