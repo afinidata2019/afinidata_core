6c883952658cf584bb9ceaaedc1797f1cfad9328
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-04-13 00:16:11 -0600

    Changed conditional for verify auth in decorator

--- app/decorators.py
+++ app/decorators.py
@@ -6,9 +6,7 @@ import os
 def check_authorization(view_func):
 
     def wrap(request, *args, **kwargs):
-        try:
-            auth = request.META['HTTP_AUTHORIZATION']
-        except Exception as e:
+        if 'HTTP_AUTHORIZATION' not in request.META:
             return HttpResponse('<h1>Unauthorized</h1>', status=403)
         encoded_credentials = request.META['HTTP_AUTHORIZATION'].split(' ')[1]
         decoded_credentials = base64.b64decode(encoded_credentials).decode("utf-8").split(':')
