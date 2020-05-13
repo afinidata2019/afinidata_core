81e79d57292fbf4827be43f5bf19cc6755c0bec5
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-05-13 13:01:40 -0600

    gitflow-hotfix-stash: attribute_value_form

--- instances/urls.py
+++ instances/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('new/', views.NewInstanceView.as_view(), name='new'),
     path('<int:id>/', views.InstanceView.as_view(), name='instance'),
     path('<int:id>/edit/', views.EditInstanceView.as_view(), name='edit'),
-    path('<int:id>/delete/', views.DeleteInstanceView.as_view(), name='delete')
+    path('<int:id>/delete/', views.DeleteInstanceView.as_view(), name='delete'),
+    path('<int:instance_id>/add_attribute/', views.AddAttributeToInstanceView.as_view(), name='add_instance_attribute'),
+    path('<int:instance_id>/edit_attribute/<int:attribute_id>/', views.AttributeValueEditView.as_view(),
+         name='edit_instance_attribute')
 ]
