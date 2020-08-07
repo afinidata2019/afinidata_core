a1b5ff614e3f514fdf7e1567992d6d1167db041f
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-08-07 15:21:55 -0600

    added libraries

--- user_sessions/views.py
+++ user_sessions/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from user_sessions import models
