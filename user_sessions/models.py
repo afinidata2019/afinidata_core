a0b50c216c62c0760db8c407221eb85de12e0ad3
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-08-10 10:15:39 -0600

    Added message form for field text.

--- user_sessions/models.py
+++ user_sessions/models.py
@@ -9,8 +9,12 @@ class Session(models.Model):
     parent_session = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
+    def __str__(self):
+        return self.name
+
 
 class Field(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
     position = models.IntegerField()
     identifier = models.UUIDField(default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -18,3 +22,28 @@ class Field(models.Model):
     field_type = models.CharField(max_length=50, choices=(('text', 'Text'), ('quick_replies', 'Quick Replies'),
                                            ('save_values_block', 'Save Values Block')))
 
+    def __str__(self):
+        return "%s" % self.identifier
+
+
+class Message(models.Model):
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Reply(models.Model):
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    label = models.CharField(max_length=30)
+    attribute = models.CharField(max_length=30, null=True, blank=True)
+    value = models.CharField(max_length=100, null=True, blank=True)
+    redirect_block = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.label
--- user_sessions/views.py
+++ user_sessions/views.py
@@ -16,6 +16,11 @@ class SessionDetailView(PermissionRequiredMixin, DetailView):
     model = models.Session
     pk_url_kwarg = 'session_id'
 
+    def get_context_data(self, **kwargs):
+        c = super(SessionDetailView, self).get_context_data()
+        c['fields'] = self.object.field_set.order_by('position')
+        return c
+
 
 class SessionCreateView(PermissionRequiredMixin, CreateView):
     permission_required = 'user_sessions.add_session'
@@ -38,8 +43,64 @@ class SessionUpdateView(PermissionRequiredMixin, UpdateView):
     fields = ('name', 'parent_session')
     pk_url_kwarg = 'session_id'
 
+    def get_context_data(self, **kwargs):
+        c = super(SessionUpdateView, self).get_context_data()
+        c['action'] = 'Edit'
+        return c
+
+    def get_success_url(self):
+        messages.success(self.request, "the session with ID: %s has updated." % self.object.pk)
+        return reverse_lazy('sessions:session_detail', kwargs=dict(session_id=self.object.pk))
+
 
 class SessionDeleteView(PermissionRequiredMixin, DeleteView):
     permission_required = 'user_sessions.delete_session'
     model = models.Session
     pk_url_kwarg = 'session_id'
+
+    def get_success_url(self):
+        messages.success(self.request, "the session with ID: %s has deleted." % self.object.pk)
+        return reverse_lazy('sessions:session_list')
+
+
+class FieldCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = 'user_sessions.add_field'
+    model = models.Field
+    fields = ('field_type', )
+
+    def get_context_data(self, **kwargs):
+        c = super(FieldCreateView, self).get_context_data()
+        c['action'] = 'Create'
+        c['session'] = models.Session.objects.get(id=self.kwargs['session_id'])
+        return c
+
+    def form_valid(self, form):
+        session = models.Session.objects.get(id=self.kwargs['session_id'])
+        form.instance.session_id = self.kwargs['session_id']
+        form.instance.position = session.field_set.count()
+        return super(FieldCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        messages.success(self.request, 'Field added to session.')
+        return reverse_lazy('sessions:session_detail', kwargs=dict(session_id=self.kwargs['session_id']))
+
+
+class MessageCreateView(PermissionRequiredMixin, CreateView):
+    permission_required = 'user_sessions.add_message'
+    model = models.Message
+    fields = ('text', )
+
+    def form_valid(self, form):
+        form.instance.field_id = self.kwargs['field_id']
+        return super(MessageCreateView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        c = super(MessageCreateView, self).get_context_data()
+        c['session'] = models.Session.objects.get(id=self.kwargs['session_id'])
+        c['field'] = models.Field.objects.get(id=self.kwargs['field_id'])
+        c['action'] = 'Create'
+        return c
+
+    def get_success_url(self):
+        messages.success(self.request, "Message added in field.")
+        return reverse_lazy('sessions:session_detail', kwargs=dict(session_id=self.kwargs['session_id']))
