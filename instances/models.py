700f69824481aedebd129b15456d87dc3e47d740
Estuardo Diaz <estuardoremi@gmail.com>
2020-08-12 14:50:55 -0600

    gitflow-hotfix-stash: session_instead_of_opened

--- instances/models.py
+++ instances/models.py
@@ -73,12 +73,12 @@ class Instance(models.Model):
         if area > 0:
             posts = Post.objects.\
                 filter(id__in=set([x.post_id for x in self.postinteraction_set \
-                                  .filter(created_at__gte=first_limit, created_at__lte=last_limit, type='opened')])) \
+                                  .filter(created_at__gte=first_limit, created_at__lte=last_limit, type='session')])) \
                 .filter(area_id=area).only('id', 'name')
         else:
             posts = Post.objects. \
                 filter(id__in=set([x.post_id for x in self.postinteraction_set \
-                                  .filter(created_at__gte=first_limit, created_at__lte=last_limit, type='opened')])) \
+                                  .filter(created_at__gte=first_limit, created_at__lte=last_limit, type='session')])) \
                 .only('id', 'name')
         return posts
 
