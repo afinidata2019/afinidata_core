c4350396f6cf05c97e59a02892f70019b7975e5d
Estuardo Diaz <estuardoremi@gmail.com>
2020-08-20 14:59:43 -0600

    gitflow-hotfix-stash: add_topic

--- articles/models.py
+++ articles/models.py
@@ -2,10 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Topic(models.Model):
+    id = models.CharField(max_length=35, primary_key=True)
+    name = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Demographic(models.Model):
+    name = models.CharField(max_length=140)
+    topics = models.ManyToManyField(Topic)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField()
     text_content = models.TextField()
+    topics = models.ManyToManyField(Topic)
     min = models.IntegerField(null=True, default=0)
     max = models.IntegerField(null=True, default=72)
     preview = models.TextField()
@@ -28,3 +49,18 @@ class Interaction(models.Model):
 
     def __str__(self):
         return "%s %s %s" % (self.article.name, self.user_id, self.type)
+
+
+class ArticleTranslate(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    language = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    text_content = models.TextField()
+    min = models.IntegerField(null=True, default=0)
+    max = models.IntegerField(null=True, default=72)
+    preview = models.TextField()
+    thumbnail = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
