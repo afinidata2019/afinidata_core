b926d54775ef201283445921b2b00a4cb617fab6
Estuardo Diaz <estuardoremi@gmail.com>
2020-09-02 13:56:12 -0600

    gitflow-hotfix-stash: sessions_interactions

--- user_sessions/models.py
+++ user_sessions/models.py
@@ -8,17 +8,46 @@ LANGS = [
 ]
 
 
+class SessionType(models.Model):
+    name = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Session(models.Model):
     name = models.CharField(max_length=100)
-    lang = models.CharField(max_length=10, choices=LANGS, default=LANGS[0][0], verbose_name='idioma')
     min = models.IntegerField(null=True, default=0, verbose_name='Min meses')
     max = models.IntegerField(null=True, default=72, verbose_name='Max meses')
+    session_type = models.ForeignKey(SessionType, on_delete=models.CASCADE, null=True)
     topics = models.ManyToManyField(Topic)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class Channels(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    channel_id = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.pk
+
+
+class Lang(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    language_id = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.pk
 
 
 class Field(models.Model):
@@ -31,6 +60,34 @@ class Field(models.Model):
 
     def __str__(self):
         return "%s" % self.pk
+
+
+class Interaction(models.Model):
+    """
+    Tracking the user interaction with the sessions
+
+    Args:
+        session:
+        user_id: ID del usuario del bot asociado a la interaction.
+        username: Username del usuario del bot asociado a la interaction (Redundancia que se utiliza para ciertos reportes).
+        channel_id: channel id del usuario del bot.
+        bot_id: Bot al cual está conectado el usuario.
+        type: String que guarda el tipo de interaction que ejecutó el usuario, estas pueden ser de cualquier tipo. De uso cotidiano en la plataforma en ciertas cosas se encuentran ‘session’ y ‘opened’, su uso puede ser muy variado.
+        value: Valor de tipo Entero que puede almacenarse en las interacciones. (Formato de Entero posiblemente temporal, guardado así por alguna necesidad, por revisar)
+        created_at, updated_at: (Uso general, fecha de creación, fecha de última actualización).
+    """
+    session = models.ForeignKey(Session, on_delete=models.CASCADE, null=True)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE, null=True)
+    user_id = models.IntegerField(default=0)
+    instance_id = models.IntegerField(default=0)
+    bot_id = models.IntegerField(default=1)
+    type = models.CharField(max_length=255, default='open')
+    value = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.type+":"+self.value
 
 
 class Message(models.Model):
@@ -54,6 +111,19 @@ class Reply(models.Model):
 
     def __str__(self):
         return self.label
+
+
+class Response(models.Model):
+    field = models.ForeignKey(Field, on_delete=models.CASCADE)
+    reply = models.ForeignKey(Reply, on_delete=models.CASCADE)
+    instance_id = models.IntegerField(default=0)
+    user_id = models.IntegerField(default=0)
+    response = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.pk)
 
 
 class RedirectBlock(models.Model):
