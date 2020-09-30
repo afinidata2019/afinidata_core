846969c992373cbee10476b44d7d4aa54b678416
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-09-29 19:18:11 -0600

    Added mockups to demo

--- utilities/urls.py
+++ utilities/urls.py
@@ -5,5 +5,9 @@ app_name = 'utilities'
 
 urlpatterns = [
     path('translate/', views.TranslateView.as_view(), name='translate'),
-    path('group_assignations/', views.GroupAssignationsView.as_view(), name='group_assignations')
+    path('group_assignations/', views.GroupAssignationsView.as_view(), name='group_assignations'),
+    path('create_program/', views.CreateProgramDemoView.as_view(), name='create_program'),
+    path('new_program/', views.NewProgramDemoView.as_view(), name='new_program'),
+    path('edit_level/', views.EditLevelDemoView.as_view(), name='edit_level'),
+    path('cognitive_example/', views.CognitiveExampleView.as_view(), name='cognitive_example')
 ]
