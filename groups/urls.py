6bdb1be2a332317d95e054127ad53f59eccec129
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-10-12 09:18:50 -0600

    Added form to add program.

--- groups/urls.py
+++ groups/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('<int:group_id>/add_code/', codes_views.CreateCodeView.as_view(), name='add_code_group'),
     path('<int:group_id>/remove_assignation/<int:assignation_id>/',
          messenger_users_views.RemoveMessengerUserView.as_view(), name='remove_user_to_group'),
-    path('add/', views.CreateGroupView.as_view(), name='add')
+    path('add/', views.CreateGroupView.as_view(), name='add'),
+    path('<int:group_id>/add_program/', views.AddProgramView.as_view(), name='add_program')
 ]
