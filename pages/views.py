21931cc56586bc5d6ec40df9a4f86a13a7af90a7
Jose Quintero <jg.fernandez31@gmail.com>
2020-10-24 10:04:21 -0400

    add post action in reset pass form

--- pages/views.py
+++ pages/views.py
@@ -55,12 +55,17 @@ class PasswordResetDoneView(TemplateView):
     template_name = 'pages/password_reset_done.html'
 
     def post(self, request, *args, **kwargs):
-        context = self.get_context_data()
-        print(context)
-        return super(TemplateView, self).render_to_response(context)
+        # TODO: generar token para cambio de constraseña
+        # TODO: guardar en base de datos
+        # TODO: enviar email
+        return super(TemplateView, self).render_to_response(self.get_context_data())
 
 class PasswordResetConfirmView(TemplateView):
+    # TODO: comprobar token
+    # TODO: mostrar formulario de cambio constraseña
+    # TODO: validar nueva contraseña y actualizar en base de datos.
     template_name = 'pages/password_reset_confirm.html'
 
 class PasswordResetCompleteView(TemplateView):
+    # TODO: mensaje de confirmacion de contraseña actualizada.
     template_name = 'pages/password_reset_complete.html'
