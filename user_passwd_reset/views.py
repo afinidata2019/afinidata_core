5d99dcbc66d1e8809150fdb081d0d0355000d0d7
Jose Quintero <jg.fernandez31@gmail.com>
2020-10-26 16:40:57 -0400

    app para password reset

--- user_passwd_reset/views.py
+++ user_passwd_reset/views.py
@@ -3,10 +3,10 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class PasswordResetView(TemplateView):
-    template_name = 'password_reset/password_reset.html'
+    template_name = 'user_passwd_reset/password_reset.html'
 
 class PasswordResetDoneView(TemplateView):
-    template_name = 'password_reset/password_reset_done.html'
+    template_name = 'user_passwd_reset/password_reset_done.html'
 
     def post(self, request, *args, **kwargs):
         # TODO: generar token para cambio de constraseña
@@ -18,12 +18,12 @@ class PasswordResetConfirmView(TemplateView):
     # TODO: comprobar token
     # TODO: mostrar formulario de cambio constraseña
     # TODO: validar nueva contraseña y actualizar en base de datos.
-    template_name = 'password_reset/password_reset_confirm.html'
+    template_name = 'user_passwd_reset/password_reset_confirm.html'
 
 
 class PasswordResetCompleteView(TemplateView):
     # TODO: mensaje de confirmacion de contraseña actualizada.
-    template_name = 'password_reset/password_reset_complete.html'
+    template_name = 'user_passwd_reset/password_reset_complete.html'
 
     def post(self, request, *args, **kwargs):
         return super(TemplateView, self).render_to_response(self.get_context_data())
