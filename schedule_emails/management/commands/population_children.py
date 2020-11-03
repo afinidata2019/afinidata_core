dc608e5401105cd849f847148a0ee38fa52a8640
Jose Quintero <jg.fernandez31@gmail.com>
2020-11-03 16:45:57 -0400

    update schedule emails command

--- schedule_emails/management/commands/population_children.py
+++ schedule_emails/management/commands/population_children.py
@@ -1,97 +1,144 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
-from schedule_emails.methods import enviar_correo
-from openpyxl import Workbook
 import os
 from core import settings
-from openpyxl.styles import Font
 from datetime import datetime
+from openpyxl import Workbook
+from openpyxl.styles import Font
+from django.db import connection
+from django.contrib.auth.models import User
+from schedule_emails.methods import enviar_correo
 from openpyxl.styles import Alignment, Font, Border, Side
+from django.core.management.base import BaseCommand, CommandError
+from openpyxl.utils import get_column_letter
 
 class Command(BaseCommand):
 
-    def create_resume(self, wb, sheet_name, header_table, data_total, data_uso, num=0):
-        ws = wb.create_sheet(sheet_name,num)
+    def create_report(self, wb):
+        heading = ('nro','región','distrito','eess','nro de niño de 0 a 24 meses','nombre','apellido paterno', 'apellido materno', 'dni','celular','total de familias','ingreso de profesional en la ultima semana')
+        ws = wb.create_sheet('Reporte')
+        ws.append(heading)
 
-        ws.column_dimensions['A'].width = 32
-        ws.column_dimensions['B'].width = 32
-        ws.column_dimensions['C'].width = 32
-        ws.column_dimensions['D'].width = 32
+        border = Border(
+            left=Side(border_style='thin', color='000000'),
+            right=Side(border_style='thin', color='000000'),
+            top=Side(border_style='thin', color='000000'),
+            bottom=Side(border_style='thin', color='000000')
+        )
 
-        thin = Side(border_style="thin", color="333333")
-        border = Border(left=thin, right=thin, top=thin, bottom=thin)
+        cursor = connection.cursor()
 
-        current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        celda = ws.cell(row=1, column=1, value=f"Afinidata {current_time}")
-        celda.font = Font(bold = True, size=12)
-        celda = ws.cell(row=2, column=1, value=f"{header_table}")
-        celda.font = Font(bold = True, size=12)
-        celda = ws.cell(row=3, column=1, value="Sobre uso familias")
-        celda.font = Font(bold = True, size=12)
+        # TODO: carmbiar por el query real
 
-        for row in data_total:
-            ws.append(row)
+        query = """
+            select * from auth_user
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
 
-        ws.insert_rows(idx=3, amount=1)
-        ws.insert_rows(idx=4, amount=1)
+        if len(result) > 0:
+            # TODO: loop de la data
+            pass
 
-        celda = ws.cell(row=10, column=1, value="Sobre uso profesionales")
-        celda.font = Font(bold = True, size=12)
+            for row in ws.iter_rows():
+                for cell in row:
+                    cell.border = border
 
-        ws.insert_rows(idx=10, amount=2)
+            for i, col in enumerate(ws.columns):
+                ws.column_dimensions[get_column_letter(i+1)].width = 25
 
-        for row in data_uso:
-            ws.append(row)
-
-        for row in ws['B13':'D13']:
-            for c in row:
-                c.alignment = Alignment(horizontal="center")
-
-        for row in ws['A5':'B9']:
-            for cell in row:
-                cell.border = border
-
-        for row in ws['A13':'D15']:
-            for cell in row:
-                cell.border = border
 
     def handle(self, *args, **kwargs):
-        # TODO: query para crear la data
 
+        archivo = os.path.join(settings.BASE_DIR, 'schedule_emails/reporte.xlsx')
         wb = Workbook()
         std=wb.get_sheet_by_name('Sheet')
         wb.remove_sheet(std)
+        ws = wb.create_sheet('Consolidado')
 
-        # TODO: loop con data real
-        data_total = [
-            ('total familias',168),
-            ('actividades totales', 908),
-            ('total de sesiones', 200),
-            ('total niños con riesgos identificados', 200),
-        ]
+        cursor = connection.cursor()
+        query = """
+        select grupo_padre.name as 'Región',
+            groups_group.name as 'Grupo',
+            count(distinct groups_assignationmessengeruser.user_id) as 'familias',
+            count(distinct posts_interaction.id) as 'actividades',
+            count(distinct user_sessions_interaction.id) as 'sesiones',
+            count(distinct case when auth_user.last_login is null
+                    then NULL else auth_user.id end) as 'Ingresaron',
+            count(distinct case when auth_user.last_login is null
+                    then auth_user.id else NULL end) as 'Pendientes'
+        from groups_group
+            left join groups_group as grupo_padre
+                on groups_group.parent_id = grupo_padre.id
+            left join groups_assignationmessengeruser
+                on groups_assignationmessengeruser.group_id = groups_group.id
+            left join user_sessions_interaction
+                on user_sessions_interaction.user_id = groups_assignationmessengeruser.user_id
+                and user_sessions_interaction.`type` = 'session_init'
+            left join posts_interaction
+                on posts_interaction.user_id = groups_assignationmessengeruser.user_id
+                and posts_interaction.`type` = 'dispatched'
+            left join groups_rolegroupuser
+                on groups_rolegroupuser.group_id = groups_group.id
+            left join auth_user
+                on groups_rolegroupuser.user_id = auth_user.id
+        group by grupo_padre.id
+        order by grupo_padre.name asc, groups_group.name asc
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
 
-        data_uso = [
-            ('','ingresaron','pendientes','% ingreso'),
-            ('Profesionales que han ingresado',21,9,70.00),
-            ('Grupos que han ingresado familias',21,7,33.33),
-        ]
+        if len(result) > 0:
+            totales = [
+                ('',),
+                ('TOTALES GENERALES',),
+                ('',),
+                ('SOBRE USO DE FAMILIAS','',''),
+                ('Total familias',sum(row[2] for row in result)),
+                ('Actividades totales', sum(row[3] for row in result)),
+                ('Total de sesiones', sum(row[4] for row in result)),
+                ('Total niños con riesgos identificados', 0),
+                ('',),
+                ('SOBRE USO DE PROFESIONALES','',''),
+                ('','Ingresaron','Pendientes','% Ingreso'),
+                ('Profesionales que han ingresado',21,9,70.00),
+                ('Grupos que han ingresado familias',21,7,33.33),
+                ('',),
+                ('',)
+            ]
 
-        for x in range(0,4):
-            if x == 0:
-                sheet_name = "Totales generales"
-                header_table = "Totales generales"
-            else:
-                sheet_name = f"title {x}"
-                header_table = f"Region {x}"
+            for row in totales:
+                ws.append(row)
 
-            self.create_resume(wb=wb,
-            num=x,
-            data_total=data_total,
-            data_uso=data_uso,
-            sheet_name=sheet_name,
-            header_table=header_table)
+            for i,row in enumerate(result):
 
+                ws.cell(row=ws.max_row+1, column=1, value="REGIÓN {0}".format(row[0]))
 
-        wb.save(os.path.join(settings.BASE_DIR, 'schedule_emails/test.xlsx'))
+                data_total = [
+                    ('',),
+                    ('SOBRE USO DE FAMILIAS','',''),
+                    ('Total familias',row[2]),
+                    ('Actividades totales', row[3]),
+                    ('Total de sesiones', row[4]),
+                    ('Total niños con riesgos identificados', 0),
+                    ('',),
+                    ('',),
+                    ('SOBRE USO DE PROFESIONALES','',''),
+                    ('','Ingresaron','Pendientes','% Ingreso'),
+                    ('Profesionales que han ingresado',21,9,70.00),
+                    ('Grupos que han ingresado familias',21,7,33.33),
+                    ('',),
+                    ('',)
+                ]
 
-        self.stdout.write('finish!')
+                for row in data_total:
+                    ws.append(row)
+
+            for i, col in enumerate(ws.columns):
+                ws.column_dimensions[get_column_letter(i+1)].width = 35
+
+            self.create_report(wb)
+
+            wb.save(archivo)
+
+            #TODO: consultar usuarios, enviar por correo con archivo adjunto.
+            enviar_correo(asunto='Poblacio de niños',template='schedule_emails/population_children.html',data=None, recipients=['correo@prueba.com'], attachment_file=archivo)
+
