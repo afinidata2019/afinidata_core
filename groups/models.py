176ee49a1f0afba34cdf0e0a648eae1667e604af
Alejandro Reyna <AleejandroReyna@gmail.com>
2020-11-11 19:54:12 -0600

    Added fields in views and models

--- groups/models.py
+++ groups/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from messenger_users.models import User as MessengerUser
+from django.utils.translation import gettext as _
 from django.contrib.auth.models import User
 from milestones.models import Milestone
 from programs.models import Program
@@ -13,6 +14,8 @@ ROLE_CHOICES = (('administrator', 'Administrator'), ('collaborator', 'Collaborat
 class Group(models.Model):
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=20, unique=True)
+    country = models.CharField(max_length=100, null=True, blank=True, verbose_name=_('País'))
+    region = models.CharField(max_length=100, null=True, blank=True, verbose_name=_('Región/Departamento/Estado'))
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     available = models.BooleanField(default=True)
