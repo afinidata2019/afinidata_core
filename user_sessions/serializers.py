2f3bbe4d478bb2dd612889e7752b008ac73acdcf
Jose Quintero <jg.fernandez31@gmail.com>
2020-11-12 17:54:14 -0400

    add changes from master branch

--- user_sessions/serializers.py
+++ user_sessions/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Field
+from .models import Field, Service
+
+class ServiceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Service
+        fields = ('__all__, serviceparam_set')
 
 class FieldSerializer(serializers.ModelSerializer):
 
+    service = ServiceSerializer(many=True)
+
     class Meta:
         model = Field
-        fields = ('id','position','field_type','field_type_display','message_set','button_set','setattribute_set','userinput_set','reply_set','condition_set','redirectblock','redirectsession')
+        fields = ('id','position','field_type','field_type_display','message_set','button_set','setattribute_set','userinput_set','reply_set','condition_set','redirectblock','redirectsession','service')
         depth = 2
