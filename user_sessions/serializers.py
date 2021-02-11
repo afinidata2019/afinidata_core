1146f58d934cfd04f7e171701f24dcdac6b2c52f
Estuardo Diaz <estuardoremi@gmail.com>
2021-02-11 09:45:28 -0600

    gitflow-hotfix-stash: sessions_sequences_get_name

--- user_sessions/serializers.py
+++ user_sessions/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import Field, Service, ServiceParam, AvailableService
+from .models import Field, Service, ServiceParam, AvailableService, AssignSequence, UnsubscribeSequence
+import requests
+import os
 
 
 class ServiceParamSerializer(serializers.ModelSerializer):
@@ -26,8 +28,40 @@ class ServiceSerializer(serializers.ModelSerializer):
         fields = ['id', 'field', 'available_service', 'serviceparam_set']
 
 
+class SubscribeSequenceSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField('get_name')
+
+    def get_name(self, obj):
+        response = requests.get(os.getenv("HOTTRIGGERS_DOMAIN_URL") + '/api/0.1/sequences/?id=' + str(obj.sequence_id))
+        name = obj.sequence_id
+        if response.status_code == 200:
+            name = "%s (%s)" % (response.json()['results'][0]['name'], response.json()['results'][0]['description'])
+        return name
+
+    class Meta:
+        model = AssignSequence
+        fields = ('id', 'field', 'sequence_id', 'name', 'start_position', 'created_at', 'updated_at')
+
+
+class UnsubscribeSequenceServiceSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField('get_name')
+
+    def get_name(self, obj):
+        response = requests.get(os.getenv("HOTTRIGGERS_DOMAIN_URL") + '/api/0.1/sequences/?id=' + str(obj.sequence_id))
+        name = obj.sequence_id
+        if response.status_code == 200:
+            name = "%s (%s)" % (response.json()['results'][0]['name'], response.json()['results'][0]['description'])
+        return name
+
+    class Meta:
+        model = UnsubscribeSequence
+        fields = ('id', 'field', 'sequence_id', 'name', 'created_at', 'updated_at')
+
+
 class FieldSerializer(serializers.ModelSerializer):
     service = ServiceSerializer()
+    assignsequence = SubscribeSequenceSerializer()
+    unsubscribesequence = UnsubscribeSequenceServiceSerializer()
 
     class Meta:
         model = Field
