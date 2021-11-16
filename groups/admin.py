1224de0c77bb1da9ca14c89b6e1da77196024835
Estuardo Díaz <estuardoremi@gmail.com>
2021-11-16 08:32:08 -0600

    Remove external files

--- groups/admin.py
+++
@@ -1,8 +0,0 @@
-from groups.models import Group, Code, AssignationMessengerUser, BotAssignation, ProgramAssignation
-from django.contrib import admin
-
-admin.site.register(Group)
-admin.site.register(Code)
-admin.site.register(AssignationMessengerUser)
-admin.site.register(ProgramAssignation)
-admin.site.register(BotAssignation)
